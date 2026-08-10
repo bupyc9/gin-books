ab4662822a60d1200ca856df0620df012ce39708
Pavel Afanasev <bupyc-pavel@yandex.ru>
2026-08-10 15:59:29 +0300

    Удалёно поле `DeletedAt`

--- src/authors/author.go
+++ src/authors/author.go
@@ -2,16 +2,13 @@ package authors
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Author struct {
-	ID         uint           `gorm:"primarykey" json:"id"`
-	CreatedAt  time.Time      `json:"createdAt"`
-	UpdatedAt  time.Time      `json:"updatedAt"`
-	DeletedAt  gorm.DeletedAt `gorm:"index" json:"-"`
-	FirstName  string         `json:"firstName"`
-	LastName   string         `json:"lastName"`
-	SecondName string         `json:"secondName"`
+	ID         uint      `gorm:"primarykey" json:"id"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
+	FirstName  string    `json:"firstName"`
+	LastName   string    `json:"lastName"`
+	SecondName string    `json:"secondName"`
 }
